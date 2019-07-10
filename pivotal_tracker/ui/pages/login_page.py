dacb95d14c1a2f9f091a1d690399fc228829334b
LGValdez <lg.valdez1995@gmail.com>
2019-07-10 09:59:57 -0400

    minor optimization

--- pivotal_tracker/ui/pages/login_page.py
+++ pivotal_tracker/ui/pages/login_page.py
@@ -2,9 +2,9 @@ from core.ui.pages.form_page import FormPage
 from core.ui.pages.action_page import ActionPage
 from pivotal_tracker.ui.pages.dashboard_page import DashboardPage
 
-username_field = 'input[id="credentials_username"]'
-password_field = 'input[id="credentials_password"]'
-signin_button = 'input[class="app_signin_action_button"]'
+username_field = '#credentials_username'
+password_field = '#credentials_password'
+signin_button = '.app_signin_action_button'
 
 
 class LoginPage(FormPage, ActionPage):
@@ -12,7 +12,7 @@ class LoginPage(FormPage, ActionPage):
         super().__init__(driver)
         fields = {
             "sign_in_as": lambda value: self.set_username(value),
-            "password": lambda value: self.set_password(value)
+            "password": lambda value: self.set_value(password_field, value)
         }
         actions = {
             "Sign In": lambda: self.sign_in()
@@ -23,9 +23,6 @@ class LoginPage(FormPage, ActionPage):
     def set_username(self, value):
         self.set_value(username_field, value)
         self.click(signin_button)
-
-    def set_password(self, value):
-        self.set_value(password_field, value)
 
     def sign_in(self):
         self.click(signin_button)
